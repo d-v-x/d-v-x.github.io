a1d78ba48c4ea0cca99b6317a40f33adb9f49204
d-v-x <arsyad.abdrahman@live.com>
2024-05-03 00:32:34 +0800

    completed functional dropdown

--- script/main.js
+++ script/main.js
@@ -1,7 +1,15 @@
+$(document).ready(function(){
+    $('#selected').html($('#li1').html());
+});
+
 function dropDownActive(){
     $('.selection').addClass('active');
 }
 
 function dropDownDeactive(){
     $('.selection').removeClass('active');
+}
+
+function selectedTopic(id){
+    $('#selected').html($('#'+id).html());
 }
