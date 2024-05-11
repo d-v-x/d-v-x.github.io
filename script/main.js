14685a94c3e2fad679d3064e4239b78b6116ca65
dvx <arsyad.abdrahman@live.com>
2024-05-11 15:27:11 +0800

    all nav item functional

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('#selected').html($('#li1').html());
+    unsetBreadcrumb('Home');
 });
 
 function dropDownActive(){
@@ -11,5 +12,32 @@ function dropDownDeactive(){
 }
 
 function selectedTopic(id){
-    $('#selected').html($('#'+id).html());
+    $('#selected').html($('#'+id).text());
+}
+
+function setBreadcrumb(text){
+    $('#breadcrumb').text(text);
+    $('#dd-container').show();
+    $('.arrow').show();
+    loadDropdown();
+}
+
+function unsetBreadcrumb(text){
+    $('#breadcrumb').text(text);
+    $('#dd-container').hide();
+    $('.arrow').hide();
+}
+
+function loadDropdown(){
+    var items = `
+                    <li id="li1" onclick="selectedTopic('li1')">topic for Photography 1</li>
+                    <li id="li2" onclick="selectedTopic('li2')">topic for Photography 2</li>
+                    <li id="li3" onclick="selectedTopic('li3')">topic for Photography 3</li>
+                    <li id="li4" onclick="selectedTopic('li4')">topic for Photography 4</li>
+                    <li id="li5" onclick="selectedTopic('li5')">topic for Photography 5</li>
+    `
+    
+    
+    $('#selectionItem').html(items);
+    selectedTopic('li1');
 }
