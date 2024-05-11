c8815af845fdae29a77c2483f6b4a9b2005bad50
dvx <arsyad.abdrahman@live.com>
2024-05-11 17:35:12 +0800

    added function to load dropdown from json

--- script/main.js
+++ script/main.js
@@ -1,43 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('#selected').html($('#li1').html());
     unsetBreadcrumb('Home');
 });
 
-function dropDownActive(){
-    $('.selection').addClass('active');
+function dropDownActive(sts) {
+    if(sts)
+        $('.selection').addClass('active');
+    else
+        $('.selection').removeClass('active');
 }
 
-function dropDownDeactive(){
-    $('.selection').removeClass('active');
+function selectedTopic(id) {
+    $('#selected').html($('#' + id).text());
 }
 
-function selectedTopic(id){
-    $('#selected').html($('#'+id).text());
-}
+function setBreadcrumb(id) {
+    var req = getRequest('/data/dropdown-items.json');
+    var items = '';
+    req = req.filter(x => x.id == id);
 
-function setBreadcrumb(text){
-    $('#breadcrumb').text(text);
+    req[0].content.forEach(element => {
+        items += '<li id="' + req[0].id + element.id + '" onclick=selectedTopic("' + req[0].id + element.id + '")>' + element.name + '</li>'
+    });
+
+    $('#selectionItem').html(items);
+    selectedTopic(req[0].id + req[0].content[0].id);
+    $('#breadcrumb').text(req[0].name);
     $('#dd-container').show();
     $('.arrow').show();
-    loadDropdown();
 }
 
-function unsetBreadcrumb(text){
+function unsetBreadcrumb(text) {
     $('#breadcrumb').text(text);
     $('#dd-container').hide();
     $('.arrow').hide();
 }
 
-function loadDropdown(){
-    var items = `
-                    <li id="li1" onclick="selectedTopic('li1')">topic for Photography 1</li>
-                    <li id="li2" onclick="selectedTopic('li2')">topic for Photography 2</li>
-                    <li id="li3" onclick="selectedTopic('li3')">topic for Photography 3</li>
-                    <li id="li4" onclick="selectedTopic('li4')">topic for Photography 4</li>
-                    <li id="li5" onclick="selectedTopic('li5')">topic for Photography 5</li>
-    `
-    
-    
-    $('#selectionItem').html(items);
-    selectedTopic('li1');
+function getRequest(url) {
+    var response_body;
+
+    $.ajax({
+        url: url,
+        async: false,
+        type: "GET",
+        success: function (data, status) {
+            response_body = data;
+        }
+    });
+
+    return response_body;
 }
